adc998f337ca8da5823669767080c1ec019b0c11
fernandosq <mastmaster@hotmail.com>
2023-10-31 12:07:54 +0100

    Implemented customer model

--- acme_financial_tool/financial_reports/models.py
+++ acme_financial_tool/financial_reports/models.py
@@ -9,3 +9,10 @@ class Order(models.Model):
         return f"ID: {self.id}, customer: {self.customer}, products: {self.products}"
 
 
+class Customer(models.Model):
+    id = models.IntegerField(primary_key=True)
+    firstname = models.CharField(max_length=32)
+    lastname = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f"Customer ID: {self.id} - Name: {self.firstname} Lastname : {self.lastname}"
