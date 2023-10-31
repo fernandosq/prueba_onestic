0e99b75dac0b44a681c22df62f6371913496201e
fernandosq <mastmaster@hotmail.com>
2023-10-31 11:34:17 +0100

    Implemented order model

--- acme_financial_tool/financial_reports/models.py
+++ acme_financial_tool/financial_reports/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    id_order = models.AutoField(primary_key=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Order ID: {self.id_order}, Total: {self.total} euros"
