7394011358c71e4954a75e1ccdcbb2a50b897cac
fernandosq <63584677+fernandosq@users.noreply.github.com>
2023-10-31 11:01:58 +0100

    Missing initial setup (#4)
    * Initializing the Django project, creating the 'acme_financial_tool' application, and creating the admin superuser
    
    * fixed traked files and add gitignore
    
    * added .idea folder into .gitignore
    
    * fix app added into settings file

--- acme_financial_tool/acme_financial_tool/settings.py
+++ acme_financial_tool/acme_financial_tool/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "acme_financial_tool"
+    "financial_reports"
 ]
 
 MIDDLEWARE = [
