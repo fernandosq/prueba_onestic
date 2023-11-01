0ce8a998a49625eaee3083792366801d3350f1af
fernandosq <63584677+fernandosq@users.noreply.github.com>
2023-11-01 19:07:29 +0100

    Reports queries (#9)
    * Implemented orders summary
    
    * Remove group_by in orders summary
    
    * Implemented product summary
    
    * Implemented generate ranking customer summary

---
+++ acme_financial_tool/financial_reports/generator_reports.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+from decimal import Decimal
+from .models import OrderCount
+
+
+def generate_orders_summary():
+    order_query = OrderCount.objects.all()
+    summary = defaultdict(Decimal)
+    for order in order_query:
+        summary[order.order.id] += order.count * order.product.cost
+    return summary
+
+
+def generate_product_summary():
+    order_query = OrderCount.objects.all()
+    summary = defaultdict(set)
+    for order in order_query:
+        summary[order.product.id].add(order.order.customer.id)
+    return {k: ",".join(str(x) for x in v) for k, v in summary.items()}
+
+
+def generate_ranking_customer_summary():
+    order_query = OrderCount.objects.all()
+    summary = defaultdict(Decimal)
+    for order in order_query:
+        summary[order.order.customer] += order.count * order.product.cost
+    return summary
+
+
+
--- acme_financial_tool/financial_reports/tests.py
+++ acme_financial_tool/financial_reports/tests.py
@@ -1,26 +1,13 @@
+from decimal import Decimal
 from django.test import TestCase
 from .data_processing import deserialize_customer_csv, deserialize_products_csv, deserialize_orders_csv
-from .models import Customer, Product, OrderCount
-
-
-# Create your tests here.
-
-
-class Products:
-    pass
+from .models import Customer, Product, OrderCount, Order
+from .generator_reports import generate_orders_summary, generate_product_summary, generate_ranking_customer_summary
 
 
 class DataProcessingTest(TestCase):
 
-    def test_read_customers(self):
-        customers = list(deserialize_customer_csv("../customers.csv"))
-        self.assertEqual(len(customers), 60)
-
-    def test_read_products(self):
-        products = list(deserialize_products_csv("../products.csv"))
-        self.assertEqual(len(products), 6)
-
-    def test_read_orders(self):
+    def setUp(self):
         for customer in deserialize_customer_csv("../customers.csv"):
             customer.save()
         self.assertEqual(Customer.objects.all().count(),60)
@@ -35,6 +22,28 @@ class DataProcessingTest(TestCase):
             for product_id in products:
                 OrderCount.add_product(order.id, product_id)
             self.assertEqual(len(set(products)), order.products.all().count())
+
+    def test_read_customers(self):
+        customers = list(deserialize_customer_csv("../customers.csv"))
+        self.assertEqual(len(customers), 60)
+
+    def test_read_products(self):
+        products = list(deserialize_products_csv("../products.csv"))
+        self.assertEqual(len(products), 6)
+
+    def test_orders_summary(self):
+        summary = generate_orders_summary()
+        self.assertEqual(len(summary), 50)
+        self.assertEqual(summary[0], Decimal("18.94"))
+
+    def test_product_summary(self):
+        summary = generate_product_summary()
+        self.assertEqual(len(summary), 6)
+
+    def test_ranking_customer_summary(self):
+        summary = generate_ranking_customer_summary()
+        self.assertEqual(len(summary), 36)
+
 
 
 
